31ef4b475a94a74c808dcf3e112666c034e1ae09
pritam65 <iampritammondal65@gmail.com>
2024-10-03 02:29:08 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import './App.css';
-import {User} from "./User";
-import {Planet} from "./Planet";
+import {useState} from "react";
 
 function App() {
-    const users = [
-        {name: "Pritam", age: 25, gender: "Male"},
-        {name: "Rahul", age: 22, gender: "Male"},
-        {name: "Arjun", age: 19, gender: "Female"},
-    ]
-
-    const planets = [
-        {name: "Earth", isGasPlanet: false},
-        {name: "Mars", isGasPlanet: false},
-        {name: "Jupiter", isGasPlanet: true},
-        {name: "Saturn", isGasPlanet: true},
-        {name: "Uranus", isGasPlanet: false},
-        {name: "Neptune", isGasPlanet: true},
-    ]
-
+   const [value, setVlaue] = useState(0);
     return (
         <div className="App">
-            <div>
-                {users.map((user, index) => {
-                return (
-                    <User
-                        key={index}
-                        name={user.name}
-                        age={user.age}
-                        gender={user.gender === "Male" ?
-                            <span style={{color: "green"}}>Male</span> :
-                            <span style={{color:'red'}}>Female</span>}
-                    />
-                )}
-            )}
-            </div>
-            <div>
-                {planets.map((planet,index) => {
-                    return (
-                        <Planet
-                        key={index}
-                        name={planet.isGasPlanet === true ?
-                        <span>{planet.name}</span> : <p>{planet.name} is Not a Gas Planet</p>}
-                        />
-                    )
-                })}
-            </div>
+          <button onClick={() => {
+              setVlaue(value + 1);
+          }}>
+              {value}
+          </button>
+
+            <button onClick={() => {
+                setVlaue(0);
+            }}>
+                Reset
+            </button>
+            <h4>{value}</h4>
         </div>
     )
 };
